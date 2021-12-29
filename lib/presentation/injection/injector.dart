884d7f9c91e2d93293453f1570c1d294c29805fa
s.kustra <skustra@mobitouch.net>
2021-12-29 15:53:18 +0100

    register dependencies

---
+++ lib/presentation/injection/injector.dart
@@ -0,0 +1,26 @@
+import 'package:astronaut/data/datasources/remote/nasa_rest_client.dart';
+import 'package:astronaut/data/repositories/pictures_repository_impl.dart';
+import 'package:astronaut/domain/usecase/get_nasa_pictures_use_case.dart';
+import 'package:astronaut/domain/usecase/get_nasa_random_pictures_use_case.dart';
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+
+final injector = GetIt.instance;
+
+Future<void> initializeDependencies() async {
+  //dio
+  injector.registerSingleton<Dio>(Dio());
+
+  //nasa rest client
+  injector.registerSingleton<NasaRestClient>(NasaRestClient(injector()));
+
+  //repositories
+  injector.registerSingleton<PicturesRepositoryImpl>(
+      PicturesRepositoryImpl(injector()));
+
+  //use case's
+  injector.registerSingleton<FetchNasaPicturesUseCase>(
+      FetchNasaPicturesUseCase(injector()));
+  injector.registerSingleton<FetchRandomNasaPicturesUseCase>(
+      FetchRandomNasaPicturesUseCase(injector()));
+}
