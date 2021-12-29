2820c6c2efddeb76108c3583ab3848fb29a14901
s.kustra <skustra@mobitouch.net>
2021-12-29 15:35:53 +0100

    return http response on rest api client methods

--- lib/data/datasources/remote/nasa_rest_client.dart
+++ lib/data/datasources/remote/nasa_rest_client.dart
@@ -6,19 +6,20 @@ import 'model/nasa_picture/remote_nasa_picture.dart';
 
 part 'nasa_rest_client.g.dart';
 
+//Generate models with 'flutter pub run build_runner build' command.
 @RestApi(baseUrl: ApiConstants.BASE_URL)
 abstract class NasaRestClient {
   factory NasaRestClient(Dio dio, {String baseUrl}) = _NasaRestClient;
 
   @GET("/planetary/apod")
-  Future<List<RemoteNasaPicture>> getNasaPictures({
+  Future<HttpResponse<List<RemoteNasaPicture>>> getNasaPictures({
     @Query("api_key") String apiKey = ApiConstants.API_URL,
     @Query("start_date") String startDate,
     @Query("end_date") String endDate,
   });
 
   @GET("/planetary/apod")
-  Future<List<RemoteNasaPicture>> getRandomNasaPictures({
+  Future<HttpResponse<List<RemoteNasaPicture>>> getRandomNasaPictures({
     @Query("api_key") String apiKey = ApiConstants.API_URL,
     @Query("count") int count,
   });
