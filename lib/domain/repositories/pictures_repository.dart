484b93ef27d06bc6d7703667a61c17fe31ea65ca
s.kustra <skustra@mobitouch.net>
2021-12-30 13:56:31 +0100

    abstraction of repositories update

--- lib/domain/repositories/pictures_repository.dart
+++ lib/domain/repositories/pictures_repository.dart
@@ -1,10 +1,18 @@
+import 'package:astronaut/data/datasources/local/entity/picture_entity.dart';
 import 'package:astronaut/data/datasources/remote/model/nasa_picture/remote_nasa_picture.dart';
 import 'package:astronaut/data/datasources/remote/state/request_state.dart';
 
 abstract class PicturesRepository {
-  Future<RequestState<List<RemoteNasaPicture>>> getNasaPictures(
+  Future<RequestState<List<RemoteNasaPicture>>> fetchNasaPictures(
       {required String startDate, required String endDate});
 
-  Future<RequestState<List<RemoteNasaPicture>>> getRandomNasaPictures(
-      {required int count});
+  Future<List<Picture>>? getSavedNasaPictures();
+
+  Future<void> insertPicture(Picture entity);
+
+  Future<void> insertPictures(List<Picture> entities);
+
+  Future<void> deletePicture(Picture entity);
+
+  Future<void> clearPictures();
 }
